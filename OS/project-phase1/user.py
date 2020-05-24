be84576f158822f0014e20d2e062cd418aa90585
AidaRamezani <aidaramezani9776@gmail.com>
2020-05-24 07:36:05 -0500

    period fixed

--- OS/project-phase1/user.py
+++ OS/project-phase1/user.py
@@ -1,17 +1,28 @@
 import subprocess
 import os
-
+import time
 n = int(input())
-
+period = int(input())
 
 command = "echo %d > /dev/getpcb" % n
 command2 = "cat /dev/getpcb > /dev/getpcb.txt"
 
+i = 0
 
-os.system(command)
-os.system(command2)
+while i < period:
+	
+
+	os.system(command)
+	os.system(command2)
 
 
 
-with open('/dev/getpcb.txt', 'r') as f:
-    print(f.read())
+
+	with open('/dev/getpcb.txt', 'r') as f:
+	    print(f.read())
+	
+	
+	i += 1
+	print("------------------------------------------------------------")
+	time.sleep(60)
+	
