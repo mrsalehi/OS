e8ef177ce1c11849f5c9ff4fef021e11568bd8f2
mrsalehi <salehi.mohammadreza1997@gmail.com>
2020-05-25 21:01:41 +0430

    Fixed user.py bug

--- OS/project-phase1/user.py
+++ OS/project-phase1/user.py
@@ -1,28 +1,24 @@
 import subprocess
 import os
 import time
-n = int(input())
-period = int(input())
+n = int(input('Input pid:'))
+period = int(input('Input period:'))
 
 command = "echo %d > /dev/getpcb" % n
 command2 = "cat /dev/getpcb > /dev/getpcb.txt"
 
 i = 0
 
-while i < period:
-	
-
+while True:
+	print('Time: %d' % i)
 	os.system(command)
 	os.system(command2)
-
-
-
 
 	with open('/dev/getpcb.txt', 'r') as f:
 	    print(f.read())
 	
 	
-	i += 1
-	print("------------------------------------------------------------")
-	time.sleep(60)
+	print("\n------------------------------------------------------------\n")
+	time.sleep(period)
+	i += period
 	
